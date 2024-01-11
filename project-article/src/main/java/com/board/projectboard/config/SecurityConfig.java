dfb0226af488400f532e3e380893808b41e95351
engh0205@naver.com <engh0205@naver.com>
2024-01-11 15:59:47 +0900

    #38  - Security적용과 테스트 작성 및 구현
    JPAConfig, SecurityConfig을 수정함
    -> 실제 인증 절차가 동작하도록 filter를 구현
    테스트 코드들의 변동사항
    * 게시글 CRUD 시 인증 필요, 이에 따른 인증되지 않은 사용자에 대한 응답까지 확인
    * 댓글 CRD 시 인증 필요
    위 사항들에 다한 테스트코드 작성과 기반 구현까지 완료

--- project-article/src/main/java/com/board/projectboard/config/SecurityConfig.java
+++ project-article/src/main/java/com/board/projectboard/config/SecurityConfig.java
@@ -1,10 +1,19 @@
 package com.board.projectboard.config;
 
 
+import com.board.projectboard.dto.UserAccountDto;
+import com.board.projectboard.dto.security.BoardPrincipal;
+import com.board.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +22,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+
                 .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests.anyRequest().permitAll()
+                        authorizeRequests.
+                                requestMatchers(
+                                        HttpMethod.GET,
+                                        "/",
+                                        "/articles",
+                                        "articles/search-hashtag"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+
+
                 )
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
                 )
+                .logout((logout) -> logout.logoutSuccessUrl("/"))
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException(("유저를 찾을 수 없습니다 username: " + username)));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
--- project-article/src/test/java/com/board/projectboard/service/ArticleCommentServiceTest.java
+++ project-article/src/test/java/com/board/projectboard/service/ArticleCommentServiceTest.java
@@ -120,19 +120,20 @@ class ArticleCommentServiceTest {
         then(articleCommentRepository).should().getReferenceById(dto.id());
     }
 
-    @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
+    @DisplayName("댓글 ID와 사용자 ID를 입력하면, 댓글을 삭제한다.")
     @Test
     void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment() {
         // Given
         Long articleCommentId = 1L;
+        String userId = "test";
 
-        willDoNothing().given(articleCommentRepository).deleteById(articleCommentId);
+        willDoNothing().given(articleCommentRepository).deleteByIdAndUserAccount_UserId(articleCommentId,userId);
 
         // When
-        sut.deleteArticleComment(articleCommentId);
+        sut.deleteArticleComment(articleCommentId,userId);
 
         // Then
-        then(articleCommentRepository).should().deleteById(articleCommentId);
+        then(articleCommentRepository).should().deleteByIdAndUserAccount_UserId(articleCommentId,userId);
     }
 
 

9f48fb1441f34d508df039ae14b3c8daad56df6e
engh0205@naver.com <engh0205@naver.com>
2024-01-15 16:05:27 +0900

    #68 - 카카오 로그인을 위한 기능 구현
    OAuth2.0 인증 방식을 도입하며 많은 변경점이 있었다.
    기존 회원가입 방식을 구현하지 않았던 부분을
    `UserAccountService`에 구현해주었다.
    카카오 로그인 응답을 파싱하기위한 responseDto를
    작성했다.
    위 두가지 기능을 구현하기 전 테스트를 정의하고 구현했다.
    `SecurityConfg`에 변경점을 반영했다.
    기존 `UserAccountRepository`에 참조하고있던 것을
    `UserAccountService`에 접근하도록 변경했다.
    OAuth2.0관련 설정을 해주었다.

--- project-article/src/test/java/com/board/projectboard/config/TestSecurityConfig.java
+++ project-article/src/test/java/com/board/projectboard/config/TestSecurityConfig.java
@@ -1,29 +1,39 @@
 package com.board.projectboard.config;
 
-import com.board.projectboard.domain.UserAccount;
+import com.board.projectboard.dto.UserAccountDto;
 import com.board.projectboard.repository.UserAccountRepository;
+import com.board.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.anyString;
+import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
     @MockBean
     private UserAccountRepository userAccountRepository;
+    @MockBean
+    private UserAccountService userAccountService;
+
 
     @BeforeTestMethod
     public void securitySetup(){
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString())).willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(),anyString(),anyString(),anyString(),anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto(){
+        return UserAccountDto.of(
                 "test",
                 "pw",
                 "test@email.com",
                 "test",
                 "test memo"
-                )
-        ));
+        );
     }
 }
